616e5fa05b25cc062cf5bef9aa3a05e41099c6e7
gameguo <1017619024@qq.com>
2021-07-02 12:30:56 +0800

    同步最新库, 接口封装修改, 添加子模块

--- Assets/UnityCamera/Core/NativeCall.cs
+++ Assets/UnityCamera/Core/NativeCall.cs
@@ -8,7 +8,7 @@ public static class NativeCall
     /// <summary>
     /// 打开相册 选择一张照片
     /// </summary>
-    public static void OpenPhoto(Action<Texture2D> callBack)
+    public static void OpenPhoto(Action<Texture2D> callBack, bool isShowToast = true)
     {
         NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((string path) =>
         {
@@ -17,17 +17,12 @@ public static class NativeCall
                 callBack(NativeGallery.LoadImageAtPath(path));
             }
         });
-        if (permission != NativeGallery.Permission.Granted)
-        {
-            ShowToast("当前没有相册访问权限，请在设置中打开");
-            //打开应用程序设置
-            if (NativeGallery.CanOpenSettings()) NativeGallery.OpenSettings();
-        }
+        CheckPermission(permission, isShowToast);
     }
     /// <summary>
     /// 打开相册 选择一张照片
     /// </summary>
-    public static void OpenPhoto(Action<string> callBack)
+    public static void OpenPhoto(Action<string> callBack, bool isShowToast = true)
     {
         NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((string path) =>
         {
@@ -36,17 +31,12 @@ public static class NativeCall
                 callBack(path);
             }
         });
-        if (permission != NativeGallery.Permission.Granted)
-        {
-            ShowToast("当前没有相册访问权限，请在设置中打开");
-            //打开应用程序设置
-            if (NativeGallery.CanOpenSettings()) NativeGallery.OpenSettings();
-        }
+        CheckPermission(permission, isShowToast);
     }
     /// <summary>
     /// 打开相册 选择多张照片
     /// </summary>
-    public static void OpenPhotos(Action<Texture2D[]> callBack)
+    public static void OpenPhotos(Action<Texture2D[]> callBack, bool isShowToast = true)
     {
         NativeGallery.Permission permission = NativeGallery.GetImagesFromGallery((string[] path) =>
         {
@@ -59,16 +49,11 @@ public static class NativeCall
                     if (!string.IsNullOrEmpty(path[i]))
                         texs[i] = NativeGallery.LoadImageAtPath(path[i]);
                 }
-                catch(Exception ex) { Debug.LogWarning("第" + i + "张图片处理失败 : " + ex.Message + "\n" + path[i]); }
+                catch (Exception ex) { Debug.LogWarning("第" + i + "张图片处理失败 : " + ex.Message + "\n" + path[i]); }
             }
             if (callBack != null) callBack(texs);
         });
-        if (permission != NativeGallery.Permission.Granted)
-        {
-            ShowToast("当前没有相册访问权限，请在设置中打开");
-            //打开应用程序设置
-            if (NativeGallery.CanOpenSettings()) NativeGallery.OpenSettings();
-        }
+        CheckPermission(permission, isShowToast);
     }
     /// <summary>
     /// 打开相册 选择多张照片
@@ -93,35 +78,25 @@ public static class NativeCall
     /// <summary>
     /// 打开相册 选择一个视频
     /// </summary>
-    public static void OpenVideo(Action<string> callBack)
+    public static void OpenVideo(Action<string> callBack, bool isShowToast = true)
     {
         NativeGallery.Permission permission = NativeGallery.GetVideoFromGallery((string path) =>
         {
             if (!string.IsNullOrEmpty(path) && callBack != null) callBack(path);
         });
-        if (permission != NativeGallery.Permission.Granted)
-        {
-            ShowToast("当前没有相册访问权限，请在设置中打开");
-            //打开应用程序设置
-            if (NativeGallery.CanOpenSettings()) NativeGallery.OpenSettings();
-        }
+        CheckPermission(permission, isShowToast);
     }
     /// <summary>
     /// 打开相册 选择多个视频
     /// </summary>
-    public static void OpenVideo(Action<string[]> callBack)
+    public static void OpenVideo(Action<string[]> callBack, bool isShowToast = true)
     {
         NativeGallery.Permission permission = NativeGallery.GetVideosFromGallery((string[] path) =>
         {
             if (path == null || path.Length == 0) return;
             if (callBack != null) callBack(path);
         });
-        if (permission != NativeGallery.Permission.Granted)
-        {
-            ShowToast("当前没有相册访问权限，请在设置中打开");
-            //打开应用程序设置
-            if (NativeGallery.CanOpenSettings()) NativeGallery.OpenSettings();
-        }
+        CheckPermission(permission, isShowToast);
     }
     #endregion
 
@@ -129,53 +104,38 @@ public static class NativeCall
     /// <summary>
     /// 打开相机拍照
     /// </summary>
-    public static void OpenCamera(Action<Texture2D> callBack)
+    public static void OpenCamera(Action<Texture2D> callBack, bool isShowToast = true)
     {
         NativeCamera.Permission permission = NativeCamera.TakePicture((string path) =>
         {
             if (!string.IsNullOrEmpty(path) && callBack != null)
                 callBack(NativeCamera.LoadImageAtPath(path));
         });
-        if (permission != NativeCamera.Permission.Granted)
-        {
-            ShowToast("当前没有相机访问权限，请在设置中打开");
-            //打开应用程序设置
-            if (NativeCamera.CanOpenSettings()) NativeCamera.OpenSettings();
-        }
+        CheckPermission(permission, isShowToast);
     }
     /// <summary>
     /// 打开相机拍照
     /// </summary>
-    public static void OpenCamera(Action<string> callBack)
+    public static void OpenCamera(Action<string> callBack, bool isShowToast = true)
     {
         NativeCamera.Permission permission = NativeCamera.TakePicture((string path) =>
         {
             if (!string.IsNullOrEmpty(path) && callBack != null)
                 callBack(path);
         });
-        if (permission != NativeCamera.Permission.Granted)
-        {
-            ShowToast("当前没有相机访问权限，请在设置中打开");
-            //打开应用程序设置
-            if (NativeCamera.CanOpenSettings()) NativeCamera.OpenSettings();
-        }
+        CheckPermission(permission, isShowToast);
     }
     /// <summary>
     /// 打开相机录像
     /// </summary>
-    public static void OpenCameraVideo(Action<string> callBack)
+    public static void OpenCameraVideo(Action<string> callBack, bool isShowToast = true)
     {
         NativeCamera.Permission permission = NativeCamera.RecordVideo((string path) =>
         {
             if (!string.IsNullOrEmpty(path) && callBack != null)
                 callBack(path);
         });
-        if (permission != NativeCamera.Permission.Granted)
-        {
-            ShowToast("当前没有相机访问权限，请在设置中打开");
-            //打开应用程序设置
-            if (NativeCamera.CanOpenSettings()) NativeCamera.OpenSettings();
-        }
+        CheckPermission(permission, isShowToast);
     }
     #endregion
 
@@ -183,90 +143,60 @@ public static class NativeCall
     /// <summary>
     /// 保存图片到相册 : 从Texture2D
     /// </summary>
-    public static void SavePhoto(Texture2D tex, Action<bool> callBack = null)
+    public static void SavePhoto(Texture2D tex, Action<bool> callBack = null, bool isShowToast = true)
     {
         try
         {
-            NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(tex, Application.productName, DateTime.Now.ToFileTime() + ".jpg", (string info) =>
+            var permission = NativeGallery.SaveImageToGallery(tex, Application.productName, DateTime.Now.ToFileTime() + ".jpg",
+                (success, p) => PhotoCallBack(success, p, callBack, isShowToast));
+            if (!CheckPermission(permission))
             {
-                if (info == null) ShowToast("保存图片成功 ! ");
-                else ShowToast("保存图片失败 : " + info);
-
-                if (callBack != null) callBack(info == null);
-            });
-            if (permission != NativeGallery.Permission.Granted)
-            {
-                ShowToast("保存图片失败 : 没有权限");
-                //打开应用程序设置
-                if (NativeGallery.CanOpenSettings()) NativeGallery.OpenSettings();
-
                 if (callBack != null) callBack(false);
             }
         }
         catch (Exception ex)
         {
-            ShowToast("保存图片失败 : " + ex.Message);
-
+            ShowLog("保存图片失败 : " + ex.Message, isShowToast);
             if (callBack != null) callBack(false);
         }
     }
     /// <summary>
     /// 保存图片到相册 : 从路径
     /// </summary>
-    public static void SavePhoto(string path, Action<bool> callBack = null)
+    public static void SavePhoto(string path, Action<bool> callBack = null, bool isShowToast = true)
     {
         try
         {
-            NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(path, Application.productName, DateTime.Now.ToFileTime() + ".jpg", (string info) =>
+            NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(path, Application.productName, DateTime.Now.ToFileTime() + ".jpg",
+                (success, p) => PhotoCallBack(success, p, callBack, isShowToast));
+            if (!CheckPermission(permission))
             {
-                if (info == null) ShowToast("保存图片成功 ! ");
-                else ShowToast("保存图片失败 : " + info);
-
-                if (callBack != null) callBack(info == null);
-            });
-            if (permission != NativeGallery.Permission.Granted)
-            {
-                ShowToast("保存图片失败 : 没有权限");
-                //打开应用程序设置
-                if (NativeGallery.CanOpenSettings()) NativeGallery.OpenSettings();
-
                 if (callBack != null) callBack(false);
             }
         }
         catch (Exception ex)
         {
-            ShowToast("保存图片失败 : " + ex.Message);
-
+            ShowLog("保存图片失败 : " + ex.Message, isShowToast);
             if (callBack != null) callBack(false);
         }
     }
     /// <summary>
     /// 保存图片到相册 : 从byte[]
     /// </summary>
-    public static void SavePhoto(byte[] data, Action<bool> callBack = null)
+    public static void SavePhoto(byte[] data, Action<bool> callBack = null, bool isShowToast = true)
     {
         try
         {
-            NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(data, Application.productName, DateTime.Now.ToFileTime() + ".jpg", (string info) =>
+            var permission = NativeGallery.SaveImageToGallery(data, Application.productName, DateTime.Now.ToFileTime() + ".jpg",
+                (success, p) => PhotoCallBack(success, p, callBack, isShowToast));
+            if (!CheckPermission(permission))
             {
-                if (info == null) ShowToast("保存图片成功 ! ");
-                else ShowToast("保存图片失败 : " + info);
-
-                if (callBack != null) callBack(info == null);
-            });
-            if (permission != NativeGallery.Permission.Granted)
-            {
-                ShowToast("保存图片失败 : 没有权限");
-                //打开应用程序设置
-                if (NativeGallery.CanOpenSettings()) NativeGallery.OpenSettings();
-
                 if (callBack != null) callBack(false);
             }
         }
         catch (Exception ex)
         {
-            ShowToast("保存图片失败 : " + ex.Message);
-
+            ShowLog("保存图片失败 : " + ex.Message, isShowToast);
             if (callBack != null) callBack(false);
         }
     }
@@ -276,66 +206,47 @@ public static class NativeCall
     /// <summary>
     /// 保存视频到相册 : 从路径
     /// </summary>
-    public static void SaveVideo(string path, Action<bool> callBack = null)
+    public static void SaveVideo(string path, Action<bool> callBack = null, bool isShowToast = true)
     {
         try
         {
-            NativeGallery.Permission permission = NativeGallery.SaveVideoToGallery(path, Application.productName, DateTime.Now.ToFileTime() + ".jpg", (string info) =>
-            {
-                if (info == null) ShowToast("保存视频成功 ! ");
-                else ShowToast("保存视频失败 : " + info);
+            NativeGallery.Permission permission = NativeGallery.SaveVideoToGallery(path, Application.productName, DateTime.Now.ToFileTime() + ".jpg",
+                (success, p) => PhotoCallBack(success, p, callBack, isShowToast));
 
-                if (callBack != null) callBack(info == null);
-            });
-            if (permission != NativeGallery.Permission.Granted)
+            if (!CheckPermission(permission))
             {
-                ShowToast("保存视频失败 : 没有权限");
-                //打开应用程序设置
-                if (NativeGallery.CanOpenSettings()) NativeGallery.OpenSettings();
-
                 if (callBack != null) callBack(false);
             }
         }
         catch (Exception ex)
         {
             ShowToast("保存视频失败 : " + ex.Message);
-
             if (callBack != null) callBack(false);
         }
     }
     /// <summary>
     /// 保存视频到相册 : 从byte[]
     /// </summary>
-    public static void SaveVideo(byte[] data, Action<bool> callBack = null)
+    public static void SaveVideo(byte[] data, Action<bool> callBack = null, bool isShowToast = true)
     {
         try
         {
-            NativeGallery.Permission permission = NativeGallery.SaveVideoToGallery(data, Application.productName, DateTime.Now.ToFileTime() + ".jpg", (string info) =>
+            NativeGallery.Permission permission = NativeGallery.SaveVideoToGallery(data, Application.productName, DateTime.Now.ToFileTime() + ".jpg",
+                (success, p) => PhotoCallBack(success, p, callBack, isShowToast));
+            if (!CheckPermission(permission))
             {
-                if (info == null) ShowToast("保存视频成功 ! ");
-                else ShowToast("保存视频失败 : " + info);
-
-                if (callBack != null) callBack(info == null);
-            });
-            if (permission != NativeGallery.Permission.Granted)
-            {
-                ShowToast("保存视频失败 : 没有权限");
-                //打开应用程序设置
-                if (NativeGallery.CanOpenSettings()) NativeGallery.OpenSettings();
-
                 if (callBack != null) callBack(false);
             }
         }
         catch (Exception ex)
         {
             ShowToast("保存视频失败 : " + ex.Message);
-
             if (callBack != null) callBack(false);
         }
     }
     #endregion
 
-    #region 显示一个Toast
+    #region Tools
 
 #if !UNITY_EDITOR && UNITY_IOS
     [System.Runtime.InteropServices.DllImport("__Internal")]
@@ -366,6 +277,89 @@ public static class NativeCall
         _showDialog(msg);
 #endif
     }
+
+    private static void ShowLog(string log, bool isShowToast)
+    {
+        if (isShowToast)
+        {
+            ShowToast(log);
+#if !UNITY_EDITOR
+        Debug.Log(log);
+#endif
+        }
+        else
+        {
+            Debug.Log(log);
+        }
+    }
+    private static void ShowLogError(string log, bool isShowToast)
+    {
+        if (isShowToast)
+        {
+            ShowToast(log);
+#if !UNITY_EDITOR
+            Debug.LogError(log);
+#endif
+        }
+        else
+        {
+            Debug.LogError(log);
+        }
+    }
     #endregion
 
+    #region 检查权限
+
+    public static bool CheckPermission(NativeGallery.Permission permission, bool isShowToast = true)
+    {
+        if (permission != NativeGallery.Permission.Granted)
+        {
+            ShowLog("当前没有相册访问权限，请在设置中打开", isShowToast);
+            //打开应用程序设置
+            if (NativeGallery.CanOpenSettings()) NativeGallery.OpenSettings();
+            return false;
+        }
+        return true;
+    }
+    public static bool CheckPermission(NativeCamera.Permission permission, bool isShowToast = true)
+    {
+        if (permission != NativeCamera.Permission.Granted)
+        {
+            ShowLog("当前没有相机访问权限，请在设置中打开", isShowToast);
+            //打开应用程序设置
+            if (NativeGallery.CanOpenSettings()) NativeGallery.OpenSettings();
+            return false;
+        }
+        return true;
+    }
+
+    #endregion
+
+    #region 保存图片与视频回调
+    private static void PhotoCallBack(bool success, string path, Action<bool> callback, bool isShowToast)
+    {
+        if (success)
+        {
+            ShowLog("保存图片成功 ! ", isShowToast);
+        }
+        else
+        {
+            ShowLog("保存图片失败 : " + path, isShowToast);
+        }
+        if (callback != null) callback(success);
+    }
+
+    private static void VideoCallBack(bool success, string path, Action<bool> callback, bool isShowToast)
+    {
+        if (success)
+        {
+            ShowLog("保存视频成功 ! ", isShowToast);
+        }
+        else
+        {
+            ShowLog("保存视频失败 : " + path, isShowToast);
+        }
+        if (callback != null) callback(success);
+    }
+    #endregion
 }
